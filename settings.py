726fe096fd93cf49b5665b29cb60b805723240f7
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-07-01 14:52:04 +0300

    замена картинки пришельцев

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
 
         """Параметры снаряда"""
         self.bullet_speed = 1
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
